e6f094222575f0148916542fba62965cbaee06bb
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-22 01:26:17 +0100

    Backport to 1.16.5

--- src/main/java/com/mrbysco/spititout/SpitItOut.java
+++ src/main/java/com/mrbysco/spititout/SpitItOut.java
@@ -1,31 +1,36 @@
 package com.mrbysco.spititout;
 
-import com.mojang.logging.LogUtils;
 import com.mrbysco.spititout.config.SpitItOutConfig;
 import io.netty.util.internal.ThrowableUtil;
-import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import org.slf4j.Logger;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 @Mod(SpitItOut.MODID)
 public class SpitItOut {
-	public static final String MODID = "spititout";
-	public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MODID = "spititout";
+    public static final Logger LOGGER = LogManager.getLogger();
 
-	public SpitItOut() {
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, SpitItOutConfig.clientSpec);
+    public SpitItOut() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, SpitItOutConfig.clientSpec);
 
-		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "Trans Rights Are Human Rights", (remoteVersionString, networkBool) -> networkBool));
-	}
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(
+                () -> "Trans Rights Are Human Rights",
+                (remoteVersionString, networkBool) -> networkBool
+        ));
+    }
 
-	public static void logThrowable(Throwable throwable) {
-		Throwable loggableThrowable = throwable;
-		if (SpitItOutConfig.CLIENT.onlyLogCause.get()) {
-			loggableThrowable = throwable.getCause();
-		}
-		SpitItOut.LOGGER.error("Exception caught in connection handler!\n {}", ThrowableUtil.stackTraceToString(loggableThrowable));
-	}
+    public static void logThrowable(Throwable throwable) {
+        Throwable loggableThrowable = throwable;
+        if (SpitItOutConfig.CLIENT.onlyLogCause.get()) {
+            loggableThrowable = throwable.getCause();
+        }
+        SpitItOut.LOGGER.error("Exception caught in connection handler!\n {}", ThrowableUtil.stackTraceToString(loggableThrowable));
+    }
 }
--- src/main/java/com/mrbysco/spititout/mixin/ConnectionMixin.java
+++ src/main/java/com/mrbysco/spititout/mixin/ConnectionMixin.java
@@ -2,37 +2,37 @@ package com.mrbysco.spititout.mixin;
 
 import com.mrbysco.spititout.SpitItOut;
 import io.netty.channel.ChannelHandlerContext;
-import net.minecraft.network.Connection;
+import net.minecraft.network.NetworkManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Connection.class)
+@Mixin(NetworkManager.class)
 public class ConnectionMixin {
 
-	@Inject(method = "exceptionCaught(Lio/netty/channel/ChannelHandlerContext;Ljava/lang/Throwable;)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/network/Connection;getCurrentProtocol()Lnet/minecraft/network/ConnectionProtocol;",
-					shift = At.Shift.BEFORE,
-					ordinal = 0
-			)
-	)
-	public void spititout_exceptionCaught(ChannelHandlerContext context, Throwable throwable, CallbackInfo ci) {
-		SpitItOut.logThrowable(throwable);
-	}
+    @Inject(method = "exceptionCaught(Lio/netty/channel/ChannelHandlerContext;Ljava/lang/Throwable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/network/NetworkManager;send(Lnet/minecraft/network/IPacket;Lio/netty/util/concurrent/GenericFutureListener;)V",
+                    shift = At.Shift.BEFORE,
+                    ordinal = 0
+            )
+    )
+    public void spititout_exceptionCaught(ChannelHandlerContext context, Throwable throwable, CallbackInfo ci) {
+        SpitItOut.logThrowable(throwable);
+    }
 
-	@Inject(method = "exceptionCaught(Lio/netty/channel/ChannelHandlerContext;Ljava/lang/Throwable;)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/network/Connection;disconnect(Lnet/minecraft/network/chat/Component;)V",
-					shift = At.Shift.BEFORE,
-					ordinal = 1
-			)
-	)
-	public void spititout_exceptionCaught2(ChannelHandlerContext context, Throwable throwable, CallbackInfo ci) {
-		SpitItOut.logThrowable(throwable);
-	}
+    @Inject(method = "exceptionCaught(Lio/netty/channel/ChannelHandlerContext;Ljava/lang/Throwable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/network/NetworkManager;disconnect(Lnet/minecraft/util/text/ITextComponent;)V",
+                    shift = At.Shift.BEFORE,
+                    ordinal = 1
+            )
+    )
+    public void spititout_exceptionCaught2(ChannelHandlerContext context, Throwable throwable, CallbackInfo ci) {
+        SpitItOut.logThrowable(throwable);
+    }
 
 }
